6d4474e701d04778d3da57ce34e4848aae044f16
cvhyd <104440549+cvhyd@users.noreply.github.com>
2022-04-27 18:56:06 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAWJNUWDSXBWGJQG7L"
-  secret_key = "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"
+  region     = "ap-south-1"
+  access_key = "AKIAU6MZSUPHHPRUWJVL"
+  secret_key = "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"
 }
